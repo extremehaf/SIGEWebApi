6b8a7499065e1dedd868f0108ad67b1705efcc91
extremehaf <lucas.de.melo.lima@gmail.com>
2018-11-23 19:29:10 -0200

    TreinamentoesController

---
+++ SIGEWebApi/Migrations/201811232127437_treinamento.cs
@@ -0,0 +1,16 @@
+namespace SIGEWebApi.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class treinamento : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
